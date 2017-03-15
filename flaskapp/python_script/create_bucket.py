80befe92594ea279d5cc09d48f0298fdcd09d74a
Shrey Mahajan <sh1994@users.noreply.github.com>
2017-03-15 13:10:23 +0530

    Delete create_bucket.py

--- flaskapp/python_script/create_bucket.py
+++
@@ -1,8 +0,0 @@
-import boto3
-import botocore
-
-s3 = boto3.resource('s3')
-
-s3.create_bucket(Bucket='bucketcloudbot.1245')
-s3.create_bucket(Bucket='bucketcloudbot.1245', CreateBucketConfiguration={
-'LocationConstraint': 'us-west-1'})
